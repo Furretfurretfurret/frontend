7fcaba290f21ba20ef7eb1fefe39d7fc976cb082
nehasq8 <neha.sq8@gmail.com>
2023-03-21 21:32:18 -0500

    added splash screen

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
-  <MainScreen />
+  <div id="app">
+    <SplashScreen :isLoading="isLoading" />
+    <main v-if="!isLoading">
+      <MainScreen />
+    </main>
+  </div>
 </template>
 
 <script>
 import MainScreen from './components/MainScreen.vue'
+import SplashScreen from './components/SplashScreen.vue';
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    MainScreen
+    MainScreen,
+    SplashScreen
+  },
+  data() {
+    return { isLoading: true };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,54 @@
+<template>
+    <div :class="{ loader: true, fadeout: !isLoading }">
+        <img src="../assets/logo.jpeg" 
+        height="300"
+        width="450"
+        style="position: relative; justify-content: center;"/>
+        <div class="bottom-div">
+            <h3>Virtualoso</h3>
+            <p style="font-style: italic; font-size: 20px;">Did you know that violins have strings?</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+name: "LoadingScreen",
+props: ["isLoading"]
+};
+</script>
+
+<style>
+.loader {
+background-color: white;
+bottom: 0;
+color: rgb(102, 63, 168);
+display: block;
+font-size: 32px;
+left: 0;
+overflow: hidden;
+padding-top: 10vh;
+position: fixed;
+right: 0;
+text-align: center;
+top: 0;
+font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+.fadeout {
+  animation: fadeout 2s forwards;
+}
+
+@keyframes fadeout {
+to {
+    opacity: 0;
+    visibility: hidden;
+}
+}
+
+.bottom-div {
+    position: fixed;
+    bottom: 15%;
+    width: 100%;
+}
+</style>
